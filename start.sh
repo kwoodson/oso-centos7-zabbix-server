86e07cc468ee7d2f16632a83dd0883aae69d7be8
Kenny Woodson <kwoodson@redhat.com>
2015-07-08 11:12:33 -0400

    fixed db connections

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@
 echo
 echo 'Ensure database exists.'
 echo '---------------'
-for i in $(seq 1 10000); do
-  sleep 10
-done
-
 /root/zabbix/createdb.sh
 echo
+#debug
+#for i in $(seq 1 10000); do
+#sleep 10
+#done
 
 echo
 echo 'Starting crond'
