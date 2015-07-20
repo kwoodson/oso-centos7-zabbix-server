fee665511edb37d955e8c8390ca9caa3c5779372
Kenny Woodson <kwoodson@redhat.com>
2015-07-20 19:29:58 -0400

    fixed

--- start.sh
+++ start.sh
@@ -7,39 +7,39 @@ sed -ri -e "s/^DBUser=.*/DBUser=${MYSQL_USER}/g" \
         -e "s/^DBHOst=.*/DBHost=${MYSQL_HOST}/g" /etc/zabbix/zabbix_server.conf
 
 
-echo
-echo 'Ensure database exists.'
-echo '---------------'
-/root/zabbix/createdb.sh
-echo
-
-# Clean up the extra templates
-echo
-echo 'Clean up zabbix templates'
-echo '---------------'
-ansible-playbook /root/ansible/playbooks/clean_zabbix.yml
-echo
-
-# Create the heartbeat and os linux templates along with items
-echo
-echo 'Create zabbix templates'
-echo '---------------'
-ansible-playbook /root/ansible/playbooks/setup_zabbix.yml
-echo
-
-echo
-echo 'Starting zabbix agent'
-echo '---------------'
-/usr/sbin/zabbix_agentd -c /etc/zabbix/zabbix_agentd.conf
-echo
-
-echo
-echo 'Starting zabbix'
-echo '---------------'
-/usr/sbin/zabbix_server -c /etc/zabbix/zabbix_server.conf
-echo
-
+#echo
+#echo 'Ensure database exists.'
+#echo '---------------'
+#/root/zabbix/createdb.sh
+#echo
+#
+## Clean up the extra templates
+#echo
+#echo 'Clean up zabbix templates'
+#echo '---------------'
+#ansible-playbook /root/ansible/playbooks/clean_zabbix.yml
+#echo
+#
+## Create the heartbeat and os linux templates along with items
+#echo
+#echo 'Create zabbix templates'
+#echo '---------------'
+#ansible-playbook /root/ansible/playbooks/setup_zabbix.yml
+#echo
+#
+#echo
+#echo 'Starting zabbix agent'
+#echo '---------------'
+#/usr/sbin/zabbix_agentd -c /etc/zabbix/zabbix_agentd.conf
+#echo
+#
+#echo
+#echo 'Starting zabbix'
+#echo '---------------'
+#/usr/sbin/zabbix_server -c /etc/zabbix/zabbix_server.conf
+#echo
+#
 while true; do
   sleep 5
 done
-
+#
