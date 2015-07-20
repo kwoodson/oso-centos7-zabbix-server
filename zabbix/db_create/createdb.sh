77d80ce2ab251fc71ee2bd11706331af5f6937bb
Kenny Woodson <kwoodson@redhat.com>
2015-07-20 19:48:17 -0400

    fixed

--- zabbix/db_create/createdb.sh
+++ zabbix/db_create/createdb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-/usr/bin/mysqlshow -u${MYSQL_USER} -h ${MYSQL_HOST} ${MYSQL_DATABASE} &>/dev/null
+/usr/bin/mysqlshow -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} ${MYSQL_DATABASE} &>/dev/null
 
 if [ "$?" == 1 ]; then
-  /usr/bin/mysql -uroot -h ${MYSQL_HOST} < /root/zabbix/create_zabbix.sql
-  /usr/bin/mysql -uroot -h ${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/schema.sql
-  /usr/bin/mysql -uroot -h ${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/images.sql
-  /usr/bin/mysql -uroot -h ${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/data.sql
+  /usr/bin/mysql -uroot -p${MYSQL_PASSWORD} -h${MYSQL_HOST} < /root/zabbix/create_zabbix.sql
+  /usr/bin/mysql -uroot -p${MYSQL_PASSWORD} -h${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/schema.sql
+  /usr/bin/mysql -uroot -p${MYSQL_PASSWORD} -h${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/images.sql
+  /usr/bin/mysql -uroot -p${MYSQL_PASSWORD} -h${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/data.sql
 else
   echo "Database ${MYSQL_DATABASE} exists.  Skipping setup."
 fi
